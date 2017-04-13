5cb74a681b66fdd6d1ff557d7b7e714f3afbd9e7
Rena Tan <rena.tanrx@gmail.com>
2017-04-13 16:27:18 -0700

    iframe install

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-
 function userSubmit(){
 //Create text variables to store data provided by user:
-
 var firstName = document.getElementById('userfname').value;
 var firstNText = document.createTextNode(firstName);
 // console.log(firstNText);
 document.getElementById('newUserName').appendChild(firstNText);
 
+
 var lastName = document.getElementById('userlname').value;
 var lastNText = document.createTextNode(" " + lastName);
 //console.log(lastNText);
 document.getElementById('newUserName').appendChild(lastNText);
+
 
 //create text variable that obtains the className for gender.
 var gender = document.getElementsByClassName('userGender');
@@ -34,26 +34,91 @@ document.getElementById('newUserGender').appendChild(genText);
 
 
 var userEmail = document.getElementById('userEmail').value;
+// console.log(userEmail);
+var userMessage = document.createTextNode(userEmail);
 var regExmail = /^[a-z0-9._%-]+@[a-z0-9.-]+\.[a-z]{2,10}$/i
-console.log(userEmail(regExmail));
+console.log(regExmail.test(userEmail));
+//Create if statement in case email address is not submitted correctly.
+if(regExmail.test(userEmail) == false){
+	alert("Please try again and submit correct email address.");
+}
+document.getElementById('newUserEmail').appendChild(userMessage);
 
-// var userPhone = document.getElementById('userPhone').value;
-// var regExphone = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/i
 
-// var userAddress = document.getElementById('userAddress').value;
+var userPhone = document.getElementById('userPhone').value;
+var regExphone = /^((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}$/
+// console.log(regExphone.test(userPhone));
+//alternative regex: ^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$
+var userPMessage = document.createTextNode(userPhone);
+document.getElementById('newUserPhone').appendChild(userPMessage);
 
-// var userCity = document.getElementById('userCity').value;
-// var regExcity = /^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/i
+  
+var userAddress = document.getElementById('userAddress').value;
+var userAddText = document.createTextNode(userAddress);
+document.getElementById('newUserAddress').appendChild(userAddText);
 
-// var userState = document.getElementById('userState').value;
-// var regExstate = /^[A-Za-z]+{2,}$/i
 
-// var userZip = document.getElementById('userZip').value;
-// var regExZip = /^\\d{5}(-\\d{4})?$/i
+var userCity = document.getElementById('userCity').value;
+var regExcity = /^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/i
+// console.log(regExcity.test(userCity));
+var userCText = document.createTextNode(userCity);
+document.getElementById('newUserCity').appendChild(userCText);
 
-// var userChoice = document.getElementById('myChoice').value;
 
-// var userCountry = document.getElementById('myCountry').value
+var userState = document.getElementById('userState').value;
+var regExstate = /^(AL|Alabama|alabama|AK|Alaska|alaska|AZ|Arizona|arizona|AR|Arkansas|arkansas|CA|California|california|CO|Colorado|colorado|CT|Connecticut|connecticut|DE|Delaware|delaware|FL|Florida|florida|GA|Georgia|georgia|HI|Hawaii|hawaii|ID|Idaho|idaho|IL|Illinois|illinois|IN|Indiana|indiana|IA|Iowa|iowa|KS|Kansas|kansas|KY|Kentucky|kentucky|LA|Louisiana|louisiana|ME|Maine|maine|MD|Maryland|maryland|MA|Massachusetts|massachusetts|MI|Michigan|michigan|MN|Minnesota|minnesota|MS|Mississippi|mississippi|MO|Missouri|missouri|MT|Montana|montana|NE|Nebraska|nebraska|NV|Nevada|nevada|NH|New Hampshire|new hampshire|NJ|New Jersey|new jersey|NM|New Mexico|new mexico|NY|New York|new york|NC|North Carolina|new carolina|ND|North Dakota|north dakota|OH|Ohio|ohio|OK|Oklahoma|oklahoma|OR|Oregon|oregon|PA|Pennsylvania|pennsylvania|RI|Rhode Island|rhode island|SC|South Carolina|south carolina|SD|South Dakota|south dakota|TN|Tennessee|tennessee|TX|Texas|texas|UT|Utah|utah|VT|Vermont|vermont|VA|Virginia|virginia|WA|Washington|washington|WV|West Virginia|west virginia|WI|Wisconsin|wisconsin|WY|Wyoming|wyoming)$/
+
+//alternative regex: ^[A-Za-z]+{2}$
+console.log(regExstate.test(userState));
+var userSText = document.createTextNode(userState);
+document.getElementById('newUserState').appendChild(userSText);
+
+
+var userZ = document.getElementById('userZip').value;
+var regExZip = /^\\d{5}(-\\d{4})?$/i
+// console.log(regExZip.test(userZip));
+var userZText = document.createTextNode(userZ);
+document.getElementById('newUserZip').appendChild(userZText);
+
+
+var userChoice = document.getElementsByClassName('userMedDropDown');
+// console.log(userChoice);
+var userMed = "";
+for(i = 0; i < userChoice.length; i++){
+	// console.log(i);
+	// console.log(userChoice[i].selected);
+	if(userChoice[i].selected){
+		userMed = userChoice[i].value
+	}
+}
+// console.log(userMed);
+var medText = document.createTextNode(userMed);
+document.getElementById('newUserMedical').appendChild(medText);
+
+
+//create text variable that obtains the className for birthplace.
+var userCountry = document.getElementsByClassName('userBirth');
+//create empty text variable that contains empty string. The
+//empty string will contain selected birthplave (from for-loop method).
+var country = "";
+//create for-loop, that loops thru the gender array (3 items) and finds
+//the ".checked" birthplace (as selected by user).
+for(i = 0; i<userCountry.length; i++){
+	if(userCountry[i].checked) {
+//create country variable for birthplace value.
+		country = userCountry[i].value
+	}
+}
+//create text variable for textNode and then append it to selected id.
+var userCText = document.createTextNode(country);
+document.getElementById('newUserBirth').appendChild(userCText);
+
+
+// var videoPic = document.createTextNode('<iframe width="560" height="315" src="https://www.youtube.com/embed/nxyMiJF32Zg" frameborder="0" allowfullscreen></iframe>');
+// document.getElementById('video').appendChild(videoPic);
+
+// var updatedProfile = document.getElementById('updatedProfile');
+updatedProfile.style.display = "block";
 
 // 	var btn = document.getElementById('sendDataButton').value
 // 	var buttonText = document.createTextNode("Thank you! Please wait while candidate profile is compiling" + btn);
